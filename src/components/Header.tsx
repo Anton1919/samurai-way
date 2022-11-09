854a0ff8b35f154854773cd4b2aeb89ac560f886
Anton1919 <bf3kenzo@mail.ru>
2022-11-09 19:42:17 +0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header className='header'>
+			<img
+				src="https://png.pngtree.com/png-vector/20190303/ourmid/pngtree-modern-abstract-3d-logo-png-image_771012.jpg"
+				alt="img"/>
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img
+					src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8="
+					alt="img"/>
+			</div>
+
+			<div>ava + descr</div>
+			<div>My posts
+				<div>
+					new post
+				</div>
+				<div>
+					<div>post 1</div>
+					<div>post 2</div>
+				</div>
+			</div>
+
+
+		</div>
+	);
+};
+
+export default Profile;
