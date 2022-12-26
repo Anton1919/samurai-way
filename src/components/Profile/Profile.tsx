0c8fe111b1b566fd795b45e65203a75f49935e46
Anton1919 <bf3kenzo@mail.ru>
2022-12-26 11:22:20 +0300

    refactoring

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 import ProFileInfo from "./ProfileInfo/ProFileInfo";
 import MyPostsContainer from "./MyPosts/MyPostContainer";
 
-// type ProfilePropsType = {
-// 	store: StoreType
-// }
-
 const Profile = () => {
 	return (
 		<div>
 			<ProFileInfo/>
-			<MyPostsContainer />
+			<MyPostsContainer/>
 		</div>
 	);
 };
