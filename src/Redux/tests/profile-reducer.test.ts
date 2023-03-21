e8be9341c1a22fe44d79b8a65a7c6b1714594f43
Anton1919 <bf3kenzo@mail.ru>
2023-03-21 12:01:30 +0300

    add unit tests (TDD)

---
+++ src/Redux/tests/profile-reducer.test.ts
@@ -0,0 +1,29 @@
+import profileReducer, {addPostActionCreator, upDateNewPostTextActionCreator} from "../profile-reducer";
+
+let initialState: any = {}
+
+beforeEach(() => {
+	initialState = {
+		posts: [
+			{id: 1, message: 'Hello', likesCount: 11},
+			{id: 2, message: 'It is my first post', likesCount: 22}
+		],
+		newPostText: '',
+		profile: null,
+		status: ''
+	}
+})
+
+test("post text should be updated", () => {
+	const action = upDateNewPostTextActionCreator("some string")
+	const newState = profileReducer(initialState, action)
+	expect(newState.newPostText).toBe("some string")
+})
+
+test("new post should be added", () => {
+	const action = addPostActionCreator()
+	const newState = profileReducer(initialState, action)
+	expect(newState.posts.at(-1)?.likesCount).toBe(0)
+	expect(newState.posts.at(-1)?.message).toBe("")
+})
+
